0649f39d1f5d4596a17b7f6b6f5fcc852ce1b888
Yingchao Ji <gusjiyid@student.gu.se>
2023-09-07 22:02:07 +0200

    #6 Return all cars Return a list of all cars

--- server/routes/car.js
+++ server/routes/car.js
@@ -15,11 +15,23 @@ var Car = mongoose.model('cars', carSchema);
 // Create a new car
 route.post('/api/cars', function(req, res, next) {
     var car = new Car(req.body);
-    car.save(function(err) {
-        if (err) { return next(err); }
-        res.status(201).json(car);
+    car.save().then(savedUser => {
+        console.log('Car save successfully:', savedUser);
+        res.json(car);
+    }).catch(err => {
+        console.error('Fail to save:', err);
     });
 });
 
+// Return a list of all cars
+route.get('/api/cars', (req, res) => {
+    Car.find({}).then(cars => {
+        console.log('All cars: ', cars);
+        res.status(200).json(cars);
+      }).catch(err => {
+        console.error('Error: ', err);
+        res.status(500).json({ error: 'Error' });
+    });
+});
 
 module.exports = route;
