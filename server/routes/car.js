886faaaabebbd6c2bc03751f2ecbe27a964c2cd9
Yingchao Ji <gusjiyid@student.gu.se>
2023-09-07 22:38:04 +0200

    #7 Update the car Update the car with the given ID

--- server/routes/car.js
+++ server/routes/car.js
@@ -47,4 +47,22 @@ route.get('/api/cars/:id', async function(req, res, next) {
     }
 });
 
+// Update the car with the given ID
+route.put('/api/cars/:id', async function(req, res, next) {
+    try {
+        const id = req.params.id;
+        const car = await Car.findById(id).exec();
+        if (car == null) {
+            return res.status(404).json({"message": "Car not found"});
+        }
+        car.image = req.body.image;
+        car.price = req.body.price;
+        car.description = req.body.description;
+        await car.save();
+        res.json(car);
+    } catch (err) {
+        return next(err);
+    }
+});
+
 module.exports = route;
