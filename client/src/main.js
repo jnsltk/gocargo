429e4512b75d48baf6158a1fea3e96522917ad7e
janos <janos@student.chalmers.se>
2023-09-21 14:05:14 +0200

    #49 Install Vue 3 and Bootstrap 5

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,15 @@
-import Vue from 'vue'
+import './assets/main.css'
+
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
 
-Vue.use(BootstrapVue)
+const app = createApp(App)
 
-Vue.config.productionTip = false
+app.use(router)
 
-new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js";
