dccb2c78c861d0f00cde26b1c05e7e8cdba2bb15
jessie <luhui930@126.com>
2023-09-25 17:25:45 +0200

    #51 add router for register

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../components/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'HomeView',
+      component: HomeView 
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      name: 'Login',
+      component:Login
+     
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: () => import('../components/Register.vue')
     }
   ]
 })
