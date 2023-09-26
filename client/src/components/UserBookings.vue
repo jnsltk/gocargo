f423334335ee70f8c585bec7cc1c54938a4963e3
janos <janos@student.chalmers.se>
2023-09-26 10:43:06 +0200

    #56 Create basic bookings page to help implementing login

--- client/src/components/UserBookings.vue
+++ client/src/components/UserBookings.vue
@@ -1,6 +1,34 @@
 <template>
-    <h1>hello</h1>
+    <div id="bookings" class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+        <div class="col mb-3" v-for="(booking, index) in bookings" :key="booking._id">
+            <div class="h-100">
+                <h2>Booking info</h2>
+                <ul>
+                    <li>Booking reference: {{ booking.bookingReference }}</li>
+                    <li>Starting date: {{ booking.startDate }}</li>
+                    <li>Ending date: {{ booking.endDate }}</li>
+                </ul>
+                <h3>Car data:</h3>
+                <img :src="booking.car.image" :alt="booking.car.brand" class="car-image">
+                <ul>
+                    <li>Brand: {{ booking.car.brand }}</li>
+                    <li>Registration: {{ booking.car.registration }}</li>
+                    <li>Description: {{ booking.car.description }}</li>
+                </ul>
+
+            </div>
+        </div>
+        <div  v-if="showNoResultsMessage" class="alert alert-warning" role="alert">
+            No results found.
+        </div>
+    </div>
 </template>
+
+<style scoped>
+    .car-image {
+        width: 300px;
+    }
+</style>
 
 <script>
     import axios from 'axios';
