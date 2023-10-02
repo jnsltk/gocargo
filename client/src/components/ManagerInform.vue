5cce4cc7a1b5aded0b885a9a3a89be2615cdfdf5
Yingchao Ji <gusjiyid@student.gu.se>
2023-10-02 21:47:24 +0200

    #59 Display manager information  Display manager information dynamically by reading from mongodb

--- client/src/components/ManagerInform.vue
+++ client/src/components/ManagerInform.vue
@@ -9,7 +9,8 @@
                     <div class="row g-3">
                         <div class="col-sm-6">
                             <label for="firstName" class="form-label fs-5">First name</label>
-                            <input type="text" class="form-control" id="firstName" placeholder="" value="Tom"  required="">
+                            <input type="text" class="form-control" id="firstName" placeholder="" v-model="manager.fname"
+                                required>
                             <div class="invalid-feedback">
                                 Valid first name is required.
                             </div>
@@ -17,7 +18,8 @@
 
                         <div class="col-sm-6">
                             <label for="lastName" class="form-label fs-5">Last name</label>
-                            <input type="text" class="form-control" id="lastName" placeholder="" value="Jerry" required="">
+                            <input type="text" class="form-control" id="lastName" placeholder="" v-model="manager.lname"
+                                required>
                             <div class="invalid-feedback">
                                 Valid last name is required.
                             </div>
@@ -26,8 +28,8 @@
                         <div class="col-12">
                             <label for="username" class="form-label fs-5">Username</label>
                             <div class="input-group has-validation">
-                                <input type="text" class="form-control" id="username" placeholder="Username"
-                                    value="TomAndJerry" required="">
+                                <input type="text" class="form-control" id="username" placeholder="" v-model="manager.email"
+                                    required>
                                 <div class="invalid-feedback">
                                     Your username is required.
                                 </div>
@@ -36,15 +38,21 @@
 
                         <div class="col-12">
                             <label for="email" class="form-label fs-5">Email </label>
-                            <input type="email" class="form-control" id="email" value="tomandjerry@gmail.com">
+                            <input type="email" class="form-control" id="email" v-model="manager.email" required>
                             <div class="invalid-feedback">
                                 Please enter a valid email address.
                             </div>
                         </div>
 
                         <div class="col-12">
+                            <label for="address" class="form-label fs-5">Balance:&nbsp;</label>
+                            <span> {{ manager.balance }}</span>
+                            <span> SEK</span>
+                        </div>
+
+                        <div class="col-12">
                             <label for="address" class="form-label fs-5">Address</label>
-                            <input type="text" class="form-control" id="address" value="Gothenburg Lindholmen" required="">
+                            <input type="text" class="form-control" id="address" v-model="manager.address" required>
                             <div class="invalid-feedback">
                                 Please enter a valid address.
                             </div>
@@ -55,7 +63,8 @@
                 </form>
             </div>
         </div>
-        <button class="w-50 btn btn-primary btn-lg" style="margin-top: 5%; margin-left: 7%;">Update Information</button>
+        <button class="w-50 btn btn-primary btn-lg" @click="updateInformation"
+            style="margin-top: 5%; margin-left: 7%;">Update Information</button>
         <div style="height: 10vh;">
 
         </div>
@@ -63,9 +72,26 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            managerEmail: 'tomandjerry@gmail.com',
+            manager: {},
+        };
+    },
+    mounted() {
+
+        axios.get(`http://localhost:3000/api/v1/managers/${this.managerEmail}`).then((response) => {
+            this.manager = response.data;
+        });
+    },
+
+    
+
+}
 
 </script>
 
-<style>
-
-</style>
+<style></style>
