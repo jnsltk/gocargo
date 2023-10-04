03cba0c610c029be7f7456737400ac6d10a2b629
Yingchao Ji <gusjiyid@student.gu.se>
2023-10-04 12:00:25 +0200

    #65 Update user account page

--- client/src/components/GetSortFilterCars.vue
+++ client/src/components/GetSortFilterCars.vue
@@ -64,7 +64,7 @@
             </div>
         </div>
     </nav>
-    <div id="fleet" class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
         <div class="col mb-3" v-for="(car, index) in cars" :key="car._id">
             <div class="h-100">
                 <CarCard :car="car" :isLastCard="index === car.length - 1"/>
--- client/src/views/ManagerView.vue
+++ client/src/views/ManagerView.vue
@@ -11,8 +11,8 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a @click="showContent('information')"  href="#" class="nav-link text-white fs-5" aria-current="page">
-                            Information
+                        <a @click="showContent('manageCars')" href="#" class="nav-link text-white fs-5">
+                            Manage cars
                         </a>
                     </li>
                     <li class="nav-item">
@@ -21,13 +21,14 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a @click="showContent('manageCars')" href="#" class="nav-link text-white fs-5">
-                            Manage cars
+                        <a @click="showContent('information')"  href="#" class="nav-link text-white fs-5" aria-current="page">
+                            Setting
                         </a>
                     </li>
+                    
                     <li class="nav-item">
                         <a href="/" class="nav-link text-white fs-5">
-                            Log out
+                            Sign out
                         </a>
                     </li>
                 </ul>
