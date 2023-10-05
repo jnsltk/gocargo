cabee1060715112daae49cbb57a20282d21d3af0
jessie <luhui930@126.com>
2023-10-05 11:56:06 +0200

    #64 add the router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -14,6 +14,7 @@ import ManageCars from '../components/ManageCars.vue'
 import RegisterView from '../views/RegisterView.vue'
 import UserAccountView from '../views/UserAccountView.vue'
 import HomeCarousel from '../components/HomeCarousel.vue'
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -102,6 +103,8 @@ const router = createRouter({
             name: 'homecarousel',
             component: HomeCarousel
         },
+       
+
     ]
 })
 
