bcd1a777a39531559555a218ff9b20e8af46046c
janos <janos@student.chalmers.se>
2023-10-06 11:24:43 +0200

    #71 Make date-picker responsive

--- client/src/components/DatePicker.vue
+++ client/src/components/DatePicker.vue
@@ -1,9 +1,17 @@
 <template>
-    <h2>Pick the starting and ending date for your rental</h2>
-    <form @submit.prevent="nextStep">
-        <div class="row">
-            <div class="col-md-6 mb-3">
-                <label for="start-date" class="form-label">Start date</label>
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5 mb-2">
+        <div class="col-10 col-sm-8 col-lg-6">
+            <img src="@/assets/pickup_stock.jpg" class="d-block mx-lg-auto img-fluid" alt="Volvo" width="700" height="500" loading="lazy">
+        </div>
+        <div class="col-lg-6">
+            <h1 class="display-5 fw-bold text-body-emphasis ">Date selection</h1>
+            <p class="lead">Please take a moment to select the dates you prefer for both the pickup and return of your rental car, ensuring a smooth and convenient booking experience.</p>
+        </div>
+    </div>
+    <form class="form" @submit.prevent="nextStep">
+        <div class="row justify-content-md-center my-4">
+            <div class="col-md-5 mb-3">
+                <label for="start-date" class="form-label lead">Pickup date</label>
                 <input 
                 type="date"
                 class="form-control"
@@ -12,8 +20,8 @@
                 required
                 />
             </div>
-            <div class="col-md-6 mb-3">
-                <label for="end-date" class="form-label">End date</label>
+            <div class="col-md-5 mb-3">
+                <label for="end-date" class="form-label lead">Return date</label>
                 <input 
                 type="date"
                 class="form-control"
@@ -23,9 +31,18 @@
                 />
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Next</button>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button type="submit" class="btn btn-outline-secondary btn-lg px-4">Previous</button>
+            <button type="submit" class="btn btn-primary btn-lg px-4">Next</button>
+        </div>
     </form>
 </template>
+
+<style scoped>
+.form {
+    min-height: 50vh;
+}
+</style>
 
 <script>
     export default {
