d0b0e5d6105f005c9a18a31aa152c09760210843
jessie <luhui930@126.com>
2023-10-07 21:42:54 +0200

    #53 fix responsive foot, add media fouction

--- client/src/components/FooterComponent.vue
+++ client/src/components/FooterComponent.vue
@@ -1,13 +1,31 @@
 <template>
     <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="/#home" class="nav-link px-2 text-body-secondary">Home</a></li>
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3 responsive-text">
+            <li class="nav-item"><a href="/#home" class="nav-link px-2 text-body-secondary ">Home</a></li>
             <li class="nav-item"><a href="/#about" class="nav-link px-2 text-body-secondary">Why us?</a></li>
-            <li class="nav-item"><a href="/#fleet" class="nav-link px-2 text-body-secondary">Fleet</a></li>
-            <li class="nav-item"><a href="/register" class="nav-link px-2 text-body-secondary">Sign up</a></li>
-            <li class="nav-item"><a href="/login" class="nav-link px-2 text-body-secondary">Log in</a></li>
+            <li class="nav-item"><a href="/#fleet" class="nav-link px-2 text-body-secondary ">Fleet</a></li>
+            <li class="nav-item"><a href="/register" class="nav-link px-2 text-body-secondary ">Sign up</a></li>
+            <li class="nav-item"><a href="/login" class="nav-link px-2 text-body-secondary ">Log in</a></li>
         </ul>
-        <p class="text-center text-body-secondary">&copy; 2023 GoCarGo, Inc</p>
+        <p class="text-center text-body-secondary copyright-text">&copy; 2023 GoCarGo, Inc</p>
     </footer>
 </template>
+
+<style>
+
+
+@media (max-width: 576px) {
+    .responsive-text{
+        font-size: 0.7rem;
+    }
+    .copyright-text{
+        font-size:0.7rem;
+    }
+}
+</style>
+
+
+
+
+
 
