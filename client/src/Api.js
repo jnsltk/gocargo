e229f1d5666375f295de44a0de3493618a110a2c
janos <janos@student.chalmers.se>
2023-10-12 21:58:26 +0200

    #76 Fix typo in url in Api.js

--- client/src/Api.js
+++ client/src/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const token = getToken();
 export const Api = axios.create({
-    baseURL: import.meta.env.VUE_APP_API_ENDPOINT || 'http://localhost:3000/api/v1',
+    baseURL: import.meta.env.VITE_APP_API_ENDPOINT || 'http://localhost:3000/api/v1',
     headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer: ' + token
