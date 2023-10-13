5789fcc8bb523e0651bdb48e50d583806f500605
janos <janos@student.chalmers.se>
2023-10-13 13:36:32 +0200

    #81 Fix dropdowns

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,75 +1,69 @@
 <template>
-    <nav id="mainNav" class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-xl">
-            <RouterLink class="navbar-brand" to='/'>GoCarGo</RouterLink>
-            <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <nav id="mainNav" class="sticky-top">
+        <BNavbar toggleable="lg" variant="dark" v-b-color-mode="'dark'" placement="fixed">
+        <BNavbarBrand href="/"><RouterLink class="navbar-brand" to='/'>GoCarGo</RouterLink></BNavbarBrand>
 
-            <div class="navbar-collapse collapse" id="navbarsExample07XL" style="">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/#home">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/#about">Why us?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/#fleet">Our fleet</a>
-                    </li>
-                </ul>
-                <div v-if="!isUserLoggedIn" class="col-md-3 text-md-end">
-                    <button type="button" @click="redirectToLogin" class="btn btn-primary me-2">Login</button>
-                    <button type="button" @click="redirectToSignUp" class="btn btn-outline-primary me-2">Sign-up</button>
-                </div>
-                <div v-else class="col-md-3 text-md-end">
-                    <a href="/useraccount" class="btn btn-primary me-2">Account</a>
-                    <button @click="logout" class="btn btn-outline-primary">Log out</button>
-                </div>
-            </div>
-        </div>
+        <BNavbarToggle target="navbar-toggle-collapse">
+        </BNavbarToggle>
+
+        <BCollapse id="navbar-toggle-collapse" is-nav>
+        <BNavbarNav class="ml-auto">
+        <BNavItem href="/#home">Home</BNavItem>
+        <BNavItem href="/#about">Why us?</BNavItem>
+        <BNavItem href="/#fleet" >Our fleet</BNavItem>
+        </BNavbarNav>
+        <BNavbarNav class="ms-auto mb-2 mb-lg-0">
+        <BNavForm v-if="!isUserLoggedIn" class="d-flex">
+        <button type="button" @click="redirectToLogin" class="btn btn-primary me-2">Login</button>
+        <button type="button" @click="redirectToSignUp" class="btn btn-outline-primary me-2">Sign-up</button>
+        </BNavForm>
+        <BNavForm v-else class="d-flex">
+        <a href="/useraccount" class="btn btn-primary me-2">Account</a>
+        <button @click="logout" class="btn btn-outline-primary">Log out</button>
+        </BNavForm>
+        </BNavbarNav>
+        </BCollapse>
+        </BNavbar>
     </nav>
 </template>
 
 <script>
-import { getToken, logout } from '../utils/auth'
-import { RouterLink } from 'vue-router'
+    import { getToken, logout } from '../utils/auth'
+    import { RouterLink } from 'vue-router'
 
 
 
 
-export default {
-    data() {
-        return {
-            isUserLoggedIn: false,
-            navbarCollapsed: true
+    export default {
+        data() {
+            return {
+                isUserLoggedIn: false,
+                navbarCollapsed: true
+            }
+        },
+        mounted() {
+            this.isUserLoggedIn = (getToken()) ? true : false;
+            console.log(this.isUserLoggedIn)
+        },
+        methods: {
+            logout() {
+                logout();
+            },
+            redirectToLogin() {
+                this.$router.push('/login');
+            },
+            redirectToSignUp() {
+                this.$router.push('/register');
+            },
+            redirecToManager() {
+                this.$router.push('/manager');
+            },
+
+        },
+        components: {
+            RouterLink
         }
-    },
-    mounted() {
-        this.isUserLoggedIn = (getToken()) ? true : false;
-        console.log(this.isUserLoggedIn)
-    },
-    methods: {
-        logout() {
-            logout();
-        },
-        redirectToLogin() {
-            this.$router.push('/login');
-        },
-        redirectToSignUp() {
-            this.$router.push('/register');
-        },
-        redirecToManager() {
-            this.$router.push('/manager');
-        },
-
-    },
-    components: {
-        RouterLink
     }
-}
 </script>
 
 <style scoped>
@@ -77,15 +71,15 @@ export default {
     padding: 10px 25px;
 }
 
-/* Responsive adjustments */
-@media (max-width: 767.98px) {
-    .gocargo-navbar {
-        height: auto;
-        /* Allow the navbar to expand vertically */
-    }
+        /* Responsive adjustments */
+        @media (max-width: 767.98px) {
+            .gocargo-navbar {
+                height: auto;
+                /* Allow the navbar to expand vertically */
+            }
 
 
-}
+        }
 </style>
 
 
--- client/src/main.js
+++ client/src/main.js
@@ -4,14 +4,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import BootstrapVueNext from 'bootstrap-vue-next'
+import 'bootstrap'
 
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(BootstrapVueNext)
 
 app.mount('#app')
 
-import "bootstrap/dist/js/bootstrap.js";
